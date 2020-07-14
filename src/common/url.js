589c1d949b04c6f9dee7b8e6c780dbfb89c4a69d
neos55555 <778007921@qq.com>
2020-07-14 15:10:18 +0800

    123

--- src/common/url.js
+++ src/common/url.js
@@ -1,5 +1,6 @@
+const isDev = process.env.NODE_ENV !== 'production';
 const url = `/api`;
-const imgUrl = `${process.env.API_HOST}:${process.env.IMG_PORT}`;
+const imgUrl = `${isDev ? process.env.API_HOST : 'http://www.uquwang.net/'}:${process.env.IMG_PORT}`;
 const serverUrl = `${process.env.API_HOST}:${process.env.PORT}${url}`
 // console.log(location)
 // 
